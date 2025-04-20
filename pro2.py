2ae4741a7b9bc196f68bf083248222f37c0c5796
shruzzn <shruti.hehel@gmail.com>
2025-04-20 18:59:48 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,7 +1,12 @@
-n=int(input("Enter the number you want to check"))
-for i in range(2,n):
-    if n%i==0:
-        print("not a prime number")
-        break
-else:
-        print('a prime number')
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2,int(n**o.5)+1):
+        if num%i==0:
+            return False
+    return True
+num=int(input('enter the number upto you want:'))
+for i in range(2,n+1):
+    if is_prime(i):
+        print(i,end='')
+        
